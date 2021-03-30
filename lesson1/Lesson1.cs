7811078d9070f1eb4e3b64997a69419084d2f71a
AlexFeather <alfeat@gmail.com>
2021-03-30 16:12:39 +0300

    Added lesson 1 solution

---
+++ lesson1/Lesson1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace level1
+{
+    class Lesson1
+    {
+        static void Main(string[] args)
+        {
+            string name = null;
+
+            Console.WriteLine("Введите свое имя:");
+            name = Console.ReadLine();
+            Console.WriteLine($"Здравствуйте, {name}. Текущее дата и время: {DateTime.Now}");
+
+        }
+    }
+}

be1fc1427ec3d90a2b5172ff37cd52cbc934fa12
AlexFeather <alfeat@gmail.com>
2021-04-01 14:43:27 +0300

    Started lesson 2

---
+++ Lesson2ex1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        static void Menu()
+        {
+
+        }
+
+        static void AverageTemp()
+        {
+
+        }
+
+        static void CurMonth()
+        {
+
+        }
+
+        static bool IsEven ()
+        {
+
+        }
+
+        static void CheckInfo()
+        {
+
+        }
+
+        static void SeasonInfo()
+        {
+
+        }
+
+        static void OfficeSchedule()
+        {
+
+        }
+    }
+
+}

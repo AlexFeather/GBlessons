1841b2eadc3e4096cc1d0416b927fd96060493d1
AlexFeather <alfeat@gmail.com>
2021-04-05 14:12:38 +0300

    Added .xml file and serializer (not tested)

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace Lesson3
 {
@@ -6,7 +8,7 @@ namespace Lesson3
     {
         static void Main(string[] args)
         {
-            DiagonalRead();
+            
         }
 
         static void Menu()
@@ -25,6 +27,63 @@ namespace Lesson3
             for (int i = 0; i < 3; i++ )
             {
                 Console.WriteLine(mass1[i,i]);
+            }
+        }
+
+        class PhoneBook
+        {
+            string[,] contacts = new string[5, 2];
+
+            static string path = @"\contactlist.xml";
+
+            static void NavMenu()
+            {
+
+            }
+
+            static void ContactList()
+            {
+
+            }
+
+            static void AddContact()
+            {
+
+            }
+
+            static void RemoveContact()
+            {
+
+            }
+
+            static void SaveList(string[,] list, string path)
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(string[,]));
+                string xml;
+                using (StringWriter stringWriter = new StringWriter())
+                {
+                    serializer.Serialize(stringWriter, list);
+                    xml = stringWriter.ToString();
+                }
+
+            }
+            static string[,] LoadList(string path)
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(string[,]));
+                string[,] loadedMass;
+                using (StreamReader streamReader = new StreamReader(path))
+                {
+                    loadedMass = (string[,])serializer.Deserialize(streamReader);
+                }
+                return loadedMass;
+            }
+
+            static void FileCheck()
+            {
+                if (!File.Exists(path))
+                {
+                    File.Create(path);
+                }
             }
         }
     }

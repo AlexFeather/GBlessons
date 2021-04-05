a1479d93a723b5c79254d8b557efe6173ca2e27f
AlexFeather <alfeat@gmail.com>
2021-04-05 13:26:10 +0300

    Added wxample 1 solution (DiagonalRead method)

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -6,7 +6,26 @@ namespace Lesson3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DiagonalRead();
+        }
+
+        static void Menu()
+        {
+
+        }
+
+        static void DiagonalRead()
+        {
+            int[,] mass1 = {
+                { 4, 8, 5},
+                { 15, 6, 1},
+                { 9, 14, 18}
+            };
+
+            for (int i = 0; i < 3; i++ )
+            {
+                Console.WriteLine(mass1[i,i]);
+            }
         }
     }
 }

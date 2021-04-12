23a371203fcac04700fde444b1d83a0ad3ff65a7
AlexFeather <alfeat@gmail.com>
2021-04-12 11:37:06 +0300

    Ex1 and Ex2 solved

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Lesson5
 {
@@ -6,7 +7,42 @@ namespace Lesson5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Ex2CurTimeToFile();
+        }
+
+        static void Ex1TextToFile()
+        {
+
+            string path = "..\\Ex1.txt";
+
+            Console.WriteLine("Введите предложение для сохранения в файл:");
+            string str = Console.ReadLine();
+
+            if(!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+            File.AppendAllText(path, str);
+            File.AppendAllText(path, Environment.NewLine);
+
+            Console.WriteLine("Запуск чтения:");
+            Console.WriteLine(File.ReadAllText(path));
+            
+        }
+
+        static void Ex2CurTimeToFile()
+        {
+            string path = "..\\startup.txt";
+
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+            File.AppendAllText(path, DateTime.Now.ToString());
+            File.AppendAllText(path, Environment.NewLine);
+
         }
     }
 }

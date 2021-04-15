0042794b28026a87c3d39ee8c3b075795954821f
AlexFeather <alfeat@gmail.com>
2021-04-15 11:53:11 +0300

    Ex1: added method that is shows process list

---
+++ Lesson6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void ProcessList()
+        {
+            Process[] Processes = Process.GetProcesses();
+            foreach(Process element in Processes)
+                Console.WriteLine($"{element.Id}: {element.ProcessName}");
+        }
+    }
+}

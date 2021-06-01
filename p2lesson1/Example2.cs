1190aa74db52975efb10f95da1997517d2fb090f
AlexFeather <alfeat@gmail.com>
2021-06-01 17:40:52 +0300

    Solved 3rd example

--- p2lesson1/Example2.cs
+++ p2lesson1/Example2.cs
@@ -9,13 +9,13 @@ namespace p2lesson1
         public static int StrangeSum(int[] inputArray) //итог: O(N^3)
         {
             int sum = 0; //можно пренебречь по третьему правилу, если длина массива больше 1
-            for (int i = 0; i < inputArray.Length; i++) //O(N^2 * N) = O(N^3)
+            for (int i = 0; i < inputArray.Length; i++) //O(N^2 * N) = O(N^3) четвертое правило
             {
-                for (int j = 0; j < inputArray.Length; j++) //O(N*N) = O (N^2)
+                for (int j = 0; j < inputArray.Length; j++) //O(N*N) = O (N^2) четвертое правило
                 {
-                    for (int k = 0; k < inputArray.Length; k++) //O(N(1+1) = O(2N), по пятому правилу двойкой можно пренебречь, так что O(N))
+                    for (int k = 0; k < inputArray.Length; k++) //O(N(1+1+1) = O(3N), по пятому правилу тройкой можно пренебречь, так что O(N), первое правило)
                     {
-                        int y = 0;
+                        int y = 0;// O(1)
 
                         if (j != 0)
                         {
---
+++ p2lesson1/Example3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace p2lesson1
+{
+    class Example3
+    {
+        static void Main()
+        {
+            Console.WriteLine(Fibonacci(7).ToString());
+
+        }
+
+        static int Fibonacci(int fn)
+        {
+            int n0 = 0;
+            int n1 = 1;
+            int n2 = 1;
+
+
+            for (int i = 3; i <= fn; i++)
+            {
+                n2 = n1 + n0;
+                n0 = n1;
+                n1 = n2;
+            }
+
+            return n2;
+        }
+    }
+}

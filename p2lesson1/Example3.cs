a2673cbce14908c446565b35979c0d761f1f12bb
AlexFeather <alfeat@gmail.com>
2021-06-01 17:49:28 +0300

    Worked out unusual inputs

--- p2lesson1/Example3.cs
+++ p2lesson1/Example3.cs
@@ -4,6 +4,11 @@ namespace p2lesson1
 {
     class Example3
     {
+        class NegativeNumberException : Exception
+        {
+            public NegativeNumberException() : base("Fibonacci number can only be greater than zero") { }
+
+        }
         static void Main()
         {
             Console.WriteLine(Fibonacci(7).ToString());
@@ -16,15 +21,22 @@ namespace p2lesson1
             int n1 = 1;
             int n2 = 1;
 
-
-            for (int i = 3; i <= fn; i++)
+            if (fn >= 2)
             {
-                n2 = n1 + n0;
-                n0 = n1;
-                n1 = n2;
-            }
+                for (int i = 3; i <= fn; i++)
+                {
+                    n2 = n1 + n0;
+                    n0 = n1;
+                    n1 = n2;
+                }
 
-            return n2;
+                return n2;
+            }
+            else if (fn == 1)
+                return n0;
+            else
+                throw new NegativeNumberException();
+
         }
     }
 }

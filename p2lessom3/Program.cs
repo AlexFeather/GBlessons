bde2e38eb2843ace025cc7ad85eb7c8151373936
AlexFeather <alfeat@gmail.com>
2021-06-10 14:49:16 +0300

    Created classes and structs needed

--- p2lessom3/Program.cs
+++ p2lessom3/Program.cs
@@ -9,10 +9,30 @@ namespace p2lesson3
             Console.WriteLine("Hello World!");
         }
 
-        public static void Distance1()
+        class LenghtInputClass
         {
-
+            public float X;
+            public float Y;
         }
 
+        struct LenghtInputStructFloat
+        {
+            public float X;
+            public float Y;
+        }
+
+        struct LenghtInputStructDouble
+        {
+            public double X;
+            public double Y;
+        }
+
+        static float Lenght_Float(LenghtInputClass input)
+        {
+            float result = MathF.Sqrt((input.X * input.X) + (input.Y * input.Y));
+            return result;
+        }
+
+        
     }
 }

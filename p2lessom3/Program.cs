806831d72c8b795c9584b2af13f07c1c68683ccc
AlexFeather <alfeat@gmail.com>
2021-06-11 12:47:23 +0300

    started benchmarking

--- p2lessom3/Program.cs
+++ p2lessom3/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace p2lesson3
 {
@@ -6,33 +10,61 @@ namespace p2lesson3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
 
-        class LenghtInputClass
+        class FloatPointClass
         {
             public float X;
             public float Y;
         }
 
-        struct LenghtInputStructFloat
+        struct FloatPointStruct
         {
             public float X;
             public float Y;
         }
 
-        struct LenghtInputStructDouble
+        struct DoublePointStruct
         {
             public double X;
             public double Y;
         }
 
-        static float Lenght_Float(LenghtInputClass input)
+
+
+
+        static float LenghtFloat(FloatPointClass input1, FloatPointClass input2)
         {
-            float result = MathF.Sqrt((input.X * input.X) + (input.Y * input.Y));
+            float Width = input1.X - input2.X;
+            float Height = input1.Y = input2.Y;
+            float result = MathF.Sqrt((Width * Width) + (Height * Height));
             return result;
         }
 
-        
+        static float LenghtFloat(FloatPointStruct input1, FloatPointStruct input2)
+        {
+            float Width = input1.X - input2.X;
+            float Height = input1.Y = input2.Y;
+            float result = MathF.Sqrt((Width * Width) + (Height * Height));
+            return result;
+        }
+
+        static double LenghtDouble(DoublePointStruct input1, DoublePointStruct input2)
+        {
+            double Width = input1.X - input2.X;
+            double Height = input1.Y = input2.Y;
+            double result = Math.Sqrt((Width * Width) + (Height * Height));
+            return result;
+        }
+
+        static double LenghtWithoutSqrt(DoublePointStruct input1, DoublePointStruct input2)
+        {
+            double Width = input1.X - input2.X;
+            double Height = input1.Y = input2.Y;
+            double result = (Width * Width) + (Height * Height);
+            return result;
+        }
     }
 }
+

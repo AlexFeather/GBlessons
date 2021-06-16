16a751f964956ea0c2abac4b8f8b1d84e18152da
AlexFeather <alfeat@gmail.com>
2021-06-16 13:32:05 +0300

    benchmarked, results stored in txt file

--- p2lessom3/Program.cs
+++ p2lessom3/Program.cs
@@ -1,70 +1,111 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace p2lesson3
+namespace p2lessom3
 {
     class Program
     {
+        public Benchmark b = new Benchmark { };
+        public static FloatPointClass classInput1 = new FloatPointClass { X = 10, Y = 140 };
+        public static FloatPointClass classInput2 = new FloatPointClass { X = 200, Y = 150 };
+        public static FloatPointStruct structFloatInput1 = new FloatPointStruct { X = 10, Y = 140 };
+        public static FloatPointStruct structFloatInput2 = new FloatPointStruct { X = 200, Y = 150 };
+        public static DoublePointStruct structDoubleInput1 = new DoublePointStruct { X = 10, Y = 140 };
+        public static DoublePointStruct structDoubleInput2 = new DoublePointStruct { X = 200, Y = 150 };
         static void Main(string[] args)
         {
-            
+            UnitTest1.TestMethod1();
         }
 
-        class FloatPointClass
+        public class FloatPointClass
         {
             public float X;
             public float Y;
         }
 
-        struct FloatPointStruct
+        public struct FloatPointStruct
         {
             public float X;
             public float Y;
         }
 
-        struct DoublePointStruct
+        public struct DoublePointStruct
         {
             public double X;
             public double Y;
         }
 
 
-
-
-        static float LenghtFloat(FloatPointClass input1, FloatPointClass input2)
+        static public float LenghtFloat(FloatPointClass input1, FloatPointClass input2)
         {
             float Width = input1.X - input2.X;
-            float Height = input1.Y = input2.Y;
+            float Height = input1.Y - input2.Y;
             float result = MathF.Sqrt((Width * Width) + (Height * Height));
             return result;
         }
 
-        static float LenghtFloat(FloatPointStruct input1, FloatPointStruct input2)
+        static public float LenghtFloat(FloatPointStruct input1, FloatPointStruct input2)
         {
             float Width = input1.X - input2.X;
-            float Height = input1.Y = input2.Y;
+            float Height = input1.Y - input2.Y;
             float result = MathF.Sqrt((Width * Width) + (Height * Height));
             return result;
         }
 
-        static double LenghtDouble(DoublePointStruct input1, DoublePointStruct input2)
+        static public double LenghtDouble(DoublePointStruct input1, DoublePointStruct input2)
         {
             double Width = input1.X - input2.X;
-            double Height = input1.Y = input2.Y;
+            double Height = input1.Y - input2.Y;
             double result = Math.Sqrt((Width * Width) + (Height * Height));
             return result;
         }
 
-        static double LenghtWithoutSqrt(DoublePointStruct input1, DoublePointStruct input2)
+        static public double LenghtWithoutSqrt(DoublePointStruct input1, DoublePointStruct input2)
         {
             double Width = input1.X - input2.X;
-            double Height = input1.Y = input2.Y;
+            double Height = input1.Y - input2.Y;
             double result = (Width * Width) + (Height * Height);
             return result;
         }
+
+    }
+
+    public class Benchmark
+    {
+        [Benchmark(Description = "Lenght Float using class")]
+        public void Measurment1()
+        {
+            Program.LenghtFloat(Program.classInput1, Program.classInput2);
+        }
+
+        [Benchmark(Description = "Lenght Float using struct")]
+        public void Measurment2()
+        {
+            Program.LenghtFloat(Program.structFloatInput1, Program.structFloatInput1);
+        }
+
+        [Benchmark(Description = "Lenght Double using struct with sqrt")]
+        public void Measurment3()
+        {
+            Program.LenghtDouble(Program.structDoubleInput1, Program.structDoubleInput1);
+        }
+
+        [Benchmark(Description = "Lenght Double using struct without sqrt")]
+        public void Measurment4()
+        {
+            Program.LenghtWithoutSqrt(Program.structDoubleInput1, Program.structDoubleInput1);
+        }
+    }
+
+    public class UnitTest1
+    {
+        public static void TestMethod1()
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
     }
 }
-

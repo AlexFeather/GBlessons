5a8893f867655fc5dd33e9c9bc1a9c5a84786dbb
AlexFeather <alfeat@gmail.com>
2021-07-29 15:15:16 +0300

    updated repo to use with dapper

--- MetricsAgent/Repos/ICpuMetricsRepository.cs
+++ MetricsAgent/Repos/ICpuMetricsRepository.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Data.SQLite;
-
+using System.Linq;
 
 namespace MetricsAgent
 {
@@ -16,97 +15,73 @@ namespace MetricsAgent
     {
         private const string ConnectionString = "Data Source=metrics.db;Version=3;Pooling=true;Max Pool Size=100;";
 
+        public CpuMetricsRepository()
+        {
+            SqlMapper.AddTypeHandler(new TimeSpanHandler());
+        }
+
         public void Create(CpuMetric item)
         {
-            using var connection = new SQLiteConnection(ConnectionString);
-            connection.Open();
-            using var cmd = new SQLiteCommand(connection);
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Execute("INSERT INTO cpumetrics(value, time) VALUES(@value, @time)",
+                new
+                {
+                    value = item.Value,
+                    time = item.Time.TotalSeconds
+                });
 
-            cmd.CommandText = "INSERT INTO cpumetrics(value, time) VALUES(@value, @time)";
-
-            cmd.Parameters.AddWithValue("@value", item.Value);
-            cmd.Parameters.AddWithValue("@time", item.Time.TotalSeconds);
-            cmd.Prepare();
-            cmd.ExecuteNonQuery();
+            }
         }
 
         public void Delete(int id)
         {
-            using var connection = new SQLiteConnection(ConnectionString);
-            connection.Open();
-            using var cmd = new SQLiteCommand(connection);
-
-            cmd.CommandText = "DELETE FROM cpumetrics WHERE id=@id";
-
-            cmd.Parameters.AddWithValue("@id", id);
-            cmd.Prepare();
-            cmd.ExecuteNonQuery();
-
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Execute("DELETE FROM cpumetrics WHERE id=@id",
+                    new
+                    {
+                        id = id
+                    });
+            };
         }
 
         public IList<CpuMetric> GetAll()
         {
-            using var connection = new SQLiteConnection(ConnectionString);
-            connection.Open();
-            using var cmd = new SQLiteCommand(connection);
-
-            cmd.CommandText = "SELECT * FROM cpumetrics";
-
-            var returnList = new List<CpuMetric>();
-
-            using (SQLiteDataReader reader = cmd.ExecuteReader())
+            using (var connection = new SQLiteConnection(ConnectionString))
             {
-                while (reader.Read())
-                {
-                    returnList.Add(new CpuMetric
-                    {
-                        Id = reader.GetInt32(0),
-                        Value = reader.GetInt32(1),
-                        Time = TimeSpan.FromSeconds(reader.GetInt32(2))
-                    });
-                }
-            }
+                return connection.Query<CpuMetric>("SELECT Id, Time, Value FROM cpumetrics").ToList();
+            };
 
-            return returnList;
         }
 
 
         public CpuMetric GetById(int id)
         {
-            using var connection = new SQLiteConnection(ConnectionString);
-            connection.Open();
-            using var cmd = new SQLiteCommand(connection);
-            cmd.CommandText = "SELECT * FROM cpumetrics WHERE id=@id";
-            using (SQLiteDataReader reader = cmd.ExecuteReader())
+            using (var connection = new SQLiteConnection(ConnectionString))
             {
-                if (reader.Read())
-                {
-                    return new CpuMetric
+                return connection.QuerySingle<CpuMetric>("SELECT Id, Time, Value FROM cpumetrics WHERE id=@id",
+                    new
                     {
-                        Id = reader.GetInt32(0),
-                        Value = reader.GetInt32(1),
-                        Time = TimeSpan.FromSeconds(reader.GetInt32(1))
-                    };
-                }
-                else
-                {
-                    return null;
-                }
+                        id = id
+                    });
             }
+           
         }
 
         public void Update(CpuMetric item)
         {
-            using var connection = new SQLiteConnection(ConnectionString);
-            using var cmd = new SQLiteCommand(connection);
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Execute("UPDATE cpumetrics SET value = @value, time = @time WHERE id=@id",
+                    new
+                    {
+                        value = item.Value,
+                        time = item.Time.TotalSeconds,
+                        id = item.Id
+                    });
+            }
 
-            cmd.CommandText = "UPDATE cpumetrics SET value = @value, time = @time WHERE id=@id;";
-            cmd.Parameters.AddWithValue("@id", item.Id);
-            cmd.Parameters.AddWithValue("@value", item.Value);
-            cmd.Parameters.AddWithValue("@time", item.Time.TotalSeconds);
-
-            cmd.Prepare();
-            cmd.ExecuteNonQuery();
         }
     }
 }
---
+++ MetricsAgent/TimeSpanHandler.cs
@@ -0,0 +1,16 @@
+﻿using Dapper;
+using System.Data;
+using System;
+
+namespace MetricsAgent
+{
+    public class TimeSpanHandler : SqlMapper.TypeHandler<TimeSpan>
+    {
+        public override TimeSpan Parse(object value)
+    => TimeSpan.FromSeconds((long)value);
+
+        public override void SetValue(IDbDataParameter parameter, TimeSpan value)
+            => parameter.Value = value;
+    }
+
+}
